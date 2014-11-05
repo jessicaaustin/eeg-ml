5c98672fde00825ccaa7979ed54ed747a160a581
Jessica Austin <jessica@aus10.org>
2014-11-04 21:55:22 -0500

    Add placeholder for iohmm implementation

---
+++ code/HMM_mat_ext/README.md
@@ -0,0 +1,4 @@
+HMM_MAT_ext
+=============
+
+Extensions to methods from HMM_MAT for IOHMM's
--- code/hmm_example/knowledge_tracing_eeg/main.m
+++ code/hmm_example/knowledge_tracing_eeg/main.m
@@ -3,8 +3,7 @@ clear;
 close all;
 
 addpath('../../HMM_mat');
-addpath('../../Barber_ML');
-addpath('../../Barber_ML_ext');
+addpath('../../HMM_mat_ext');
 
 %% Generate observations and plot results
 
@@ -14,7 +13,7 @@ x = {};
 xcolors = {'rx', 'gx'};
 scolors = {'yo', 'ko'};
     
-% figure; hold on;
+figure; hold on;
 
 for w=1:100
     
@@ -42,61 +41,38 @@ b0 = [.5  .5
 p0 = [0.9; 
            0.1];
 
-fprintf('Estimating parameters using EM (Baum Welch) MATLAB implementation:\n');
-[A_est_EM_matlab, b_est_EM_matlab] = hmmtrain(x, A0, b0);
-
-fprintf('Estimating parameters using EM (Baum Welch) HMM Toolbox implementation:\n');
-[LL, p_est_EM_HMMt, A_est_EM_HMMt, b_est_EM_HMMt] = learn_dhmm(x, p0, A0, b0, 50, 1E-8);
-
-fprintf('Estimating parameters using EM (Baum Welch) Barber implementation:\n');
-opts.maxit = 50;
-opts.plotprogress = 0; % figure;
-[A_est_EM_Barber, p_est_EM_Barber, b_est_EM_Barber, ~] = HMMem(x, size(A0,2), size(b0,2), opts);
+fprintf('Estimating parameters using EM (Baum Welch) HMM Toolbox ext implementation:\n');
+[LL, p_est_EM_HMMt, A_est_EM_HMMt, b_est_EM_HMMt] = learn_dhmm_iohmm(x, p0, A0, b0, 50, 1E-8);
 
 %% Plot results
 
 figure;
-subplot(2,2,1); bar(p_true, 'r'); title('true initial p');
-subplot(2,2,3); bar(p_est_EM_HMMt); title('learned initial p (HMM Toolbox)');
-subplot(2,2,4); bar(p_est_EM_Barber); title('learned initial p (Barber)');
+subplot(1,2,1); bar(p_true, 'r'); title('true initial p');
+subplot(1,2,2); bar(p_est_EM_HMMt); title('learned initial p (HMM Toolbox)');
 
 figure;
-subplot(2,2,1); imagesc(A_true); title('true transition');
-subplot(2,2,2); imagesc(A_est_EM_matlab); title('learned transition (MATLAB hmmtrain)');
-subplot(2,2,3); imagesc(A_est_EM_HMMt); title('learned transition (HMM Toolbox)');
-subplot(2,2,4); imagesc(A_est_EM_Barber); title('learned transition (Barber)');
+subplot(1,2,1); imagesc(A_true); title('true transition');
+subplot(1,2,2); imagesc(A_est_EM_HMMt); title('learned transition (HMM Toolbox)');
 
 figure;
-subplot(2,2,1); imagesc(b_true); title('true emission');
-subplot(2,2,2); imagesc(b_est_EM_matlab); title('learned emission (MATLAB hmmtrain)');
-subplot(2,2,3); imagesc(b_est_EM_HMMt); title('learned emission (HMM Toolbox)');
-subplot(2,2,4); imagesc(b_est_EM_Barber); title('learned emission (Barber)');
+subplot(1,2,1); imagesc(b_true); title('true emission');
+subplot(1,2,2); imagesc(b_est_EM_HMMt); title('learned emission (HMM Toolbox)');
 
 
 %% Print out results
 
-Methods = {'Actual Value', 'MATLAB hmmtrain', 'HMM Toolbox', 'Barber'};
+Methods = {'Actual Value', 'HMM Toolbox'};
 
 l0 = [p_true(2); 
-      nan;
-      p_est_EM_HMMt(2);
-      p_est_EM_Barber(2)];
+      p_est_EM_HMMt(2)];
 p_learn =  [A_true(1,2); 
-            A_est_EM_matlab(1,2); 
-            A_est_EM_HMMt(1,2); 
-            A_est_EM_Barber(1,2)];
+            A_est_EM_HMMt(1,2)];
 p_forget = [A_true(2,1);
-            A_est_EM_matlab(2,1);
-            A_est_EM_HMMt(2,1);
-            A_est_EM_Barber(2,1)];
+            A_est_EM_HMMt(2,1)];
 p_guess = [b_true(1,2);
-           b_est_EM_matlab(1,2);
-           b_est_EM_HMMt(1,2);
-           b_est_EM_Barber(1,2)];
+           b_est_EM_HMMt(1,2)];
 p_slip = [b_true(2,1);
-          b_est_EM_matlab(2,1);
-          b_est_EM_HMMt(2,1);
-          b_est_EM_Barber(2,1)];
+          b_est_EM_HMMt(2,1)];
 
 T = table(l0, p_learn, p_forget, p_guess, p_slip, ...
     'RowNames', Methods)
