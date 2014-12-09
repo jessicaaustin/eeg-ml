bc0a7651cb5f8dcff173a9d18a4e292665e92900
Jessica Austin <jessica@aus10.org>
2014-12-09 16:02:54 -0500

    Correlate _normalized_ time and eeg attention

--- code/classifier/kt_attn/correlateTimeElapsedAndAttention.m
+++ code/classifier/kt_attn/correlateTimeElapsedAndAttention.m
@@ -4,6 +4,7 @@ close all;
 load('subjects.mat');
 N = length(subjectids);
 
+all_p = [];
 
 for i=1:N
     sid=subjectids{i};
@@ -14,7 +15,9 @@ for i=1:N
     
     haseeg = boolean(data.haseeg);
     timeelapsed=data.timeelapsedms(haseeg);
+    timeelapsed = timeelapsed/mean(double(timeelapsed));
     attention=data.attention(haseeg);
+    attention = attention/mean(double(attention));
     meditation=data.meditation(haseeg);
     
 %     figure; hold on;
@@ -24,4 +27,18 @@ for i=1:N
 %         plot(timeelapsed, p.*timeelapsed, 'g--');
 %         ylim([min(attention), max(attention)]);
         fprintf('%0.2f\n', p);
+        
+        all_p = [all_p; p];
 end
+
+if false
+    figure; hold on;
+        plot(timeelapsed, attention, 'bo');
+        p=double(timeelapsed)\double(attention);
+        plot(timeelapsed, p.*timeelapsed, 'go--');
+%         ylim([min(attention), max(attention)]);
+        fprintf('%0.2f\n', p);
+end
+
+fprintf('Time elapsed and attention are correlated with r = %0.2f +/- %0.4f\n', ...
+    mean(all_p), std(all_p));
